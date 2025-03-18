dde7dbbe3ce73963e8b404ae3ce12e024977baac
Amsterdan Vasconcelos <amsterdan.contato@gmail.com>
2025-03-18 13:24:22 -0300

    feat: create sign-in sign-up pages

--- environment.d.ts
+++ environment.d.ts
@@ -3,6 +3,10 @@ declare global {
     interface ProcessEnv {
       NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: string;
       CLERK_SECRET_KEY: string;
+      NEXT_PUBLIC_CLERK_SIGN_IN_URL: string;
+      NEXT_PUBLIC_CLERK_SIGN_UP_URL: string;
+      NEXT_PUBLIC_CLERK_SIGN_IN_FALLBACK_REDIRECT_URL: string;
+      NEXT_PUBLIC_CLERK_SIGN_UP_FALLBACK_REDIRECT_URL: string;
     }
   }
 }
--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,7 +1,12 @@
+import { SignedIn, UserButton } from '@clerk/nextjs';
+
 export default function Home() {
   return (
     <div>
       <h1>HomePage</h1>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </div>
   );
 }
